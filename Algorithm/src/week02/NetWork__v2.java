1fc013c2b130e7ff6a3da445b9001a70d4c24de5
yeonhookim <rladusgn68@gmail.com>
2025-04-27 20:56:22 +0900

    2025_04_26_NetWork_V2

---
+++ Algorithm/src/week02/NetWork__v2.java
@@ -0,0 +1,18 @@
+import java.util.stream.IntStream;
+
+class NetWork {
+    public int solution(int n, int[][] computers) {
+        boolean[] visited = new boolean[n];
+        return (int) IntStream.range(0, n)
+                .filter(i -> !visited[i] && (dfs(i, computers, visited) || true))
+                .count();
+    }
+
+    private boolean dfs(int current, int[][] computers, boolean[] visited) {
+        visited[current] = true;
+        IntStream.range(0, computers.length)
+                .filter(neighbor -> computers[current][neighbor] == 1 && !visited[neighbor])
+                .forEach(neighbor -> dfs(neighbor, computers, visited));
+        return true;
+    }
+}
