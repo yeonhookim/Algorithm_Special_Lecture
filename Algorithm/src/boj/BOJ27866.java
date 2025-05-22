5d5e02937e8a4f6d8ce4af8caaf1aa207b8d1d94
yeonhookim <rladusgn68@gmail.com>
2025-05-22 17:31:29 +0900

    2025_05_22v5

---
+++ Algorithm/src/boj/BOJ27866.java
@@ -0,0 +1,17 @@
+package boj;
+
+import java.util.Scanner;
+
+public class BOJ27866 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String S = sc.nextLine();
+        int i = sc.nextInt();
+
+        System.out.println(S.charAt(i-1));
+
+
+    }
+}
