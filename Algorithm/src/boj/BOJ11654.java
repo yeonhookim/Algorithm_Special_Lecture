f5499bd685d984d021e5dff51e08c49622e265b5
yeonhookim <rladusgn68@gmail.com>
2025-05-22 16:53:51 +0900

    2025_05_22v4

---
+++ Algorithm/src/boj/BOJ11654.java
@@ -0,0 +1,14 @@
+package boj;
+
+import java.util.Scanner;
+
+public class BOJ11654 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char input = sc.nextLine().charAt(0);
+
+        System.out.println((int)input);
+    }
+}
